668baa61fec77326820491402e1c4185fae26b87
hardipsolanki22 <hardipsolanki2224@gmail.com>
2025-02-13 14:42:34 +0530

    update axios base URL to production API endpoint

--- Frontend/src/helpers/axiosService.js
+++ Frontend/src/helpers/axiosService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:8080/api/v1"
+// const baseURL = "http://localhost:8080/api/v1"
+const baseURL = "https://simple-ecommerce-site-api.onrender.com/api/v1"
 
 const axiosInstance = axios.create({
         baseURL
